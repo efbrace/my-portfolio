032e29c9fd69f9e9b01c0077cd5dc0b9d37ed8ad
efbrace <ebenezerflintwoodbrace@gmail.com>
2025-07-20 17:45:04 +0000

    ""

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -18,7 +18,7 @@ const InputField = ({
   rows = 3,
 }: InputFieldProps) => {
   const baseClasses =
-    "lg:w-[35%] sm:w-[50%] w-[90%] max-w-[400px]  p-2 text-base bg-transparent border-l-2 border-b-2 border-black/40 transition-all placeholder:uppercase placeholder:text-xs placeholder:font-bold placeholder:tracking-wider focus:w-[100%] hover:bg-White/50 focus:bg-White/50 shadow-sm hover:shadow focus:shadow outline-none";
+    "lg:w-[35%] sm:w-[50%] w-[90%] max-w-[400px]  p-2 text-base bg-transparent border-l-2 border-b-2 border-black/40 transition-all placeholder:text-Black/60 placeholder:uppercase placeholder:text-xs placeholder:font-bold placeholder:tracking-wider focus:w-[100%] hover:bg-White/50 focus:bg-White/50 shadow-sm hover:shadow focus:shadow outline-none";
 
   if (isTextarea) {
     return (
